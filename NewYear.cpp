c6801244efddd847eceacda781c77121772ad741
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-19 12:00:05 +0600

    total push-up count.

---
+++ NewYear.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int X){
+    cout << X * 31 << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X; cin >> X;
+
+    solve(X);
+
+    return 0;
+}
