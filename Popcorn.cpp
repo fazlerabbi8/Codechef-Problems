b852a8ae2ba74dde7a039ef56407f74349a931ab
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-19 12:01:12 +0600

    total popcorn can buy after spend on ticket.

--- Popcorn.cpp
+++ Popcorn.cpp
@@ -1,25 +1,25 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// void solve(int X)
-// {
-//     if (X >= 100)
-//     {
-//         int leftOverMoney = X - 100;
-//         int popcornCanBuy = leftOverMoney / 50;
-//         cout << popcornCanBuy << "\n";
-//     }
-// }
+void solve(int X)
+{
+    if (X >= 100)
+    {
+        int leftOverMoney = X - 100;
+        int popcornCanBuy = leftOverMoney / 50;
+        cout << popcornCanBuy << "\n";
+    }
+}
 
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-//     int X;
-//     cin >> X;
+    int X;
+    cin >> X;
 
-//     solve(X);
+    solve(X);
 
-//     return 0;
-// }
+    return 0;
+}
