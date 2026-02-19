97d785faaac67fa3bd814927bd811db52f25e9dc
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-19 12:02:50 +0600

    who is the ultimate winner.

--- Rivalry.cpp
+++ Rivalry.cpp
@@ -1,28 +1,28 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// void solve(int R1, int R2, int D1, int D2)
-// {
-//     int Dominater = R1 + (D1);
-//     int Everule = R2 + (D2);
+void solve(int R1, int R2, int D1, int D2)
+{
+    int Dominater = R1 + (D1);
+    int Everule = R2 + (D2);
 
-//     if(Dominater > Everule){
-//         cout << "Dominater" << "\n";
-//     }
-//     if(Dominater < Everule){
-//         cout << "Everule" << "\n";
-//     }
-// }
+    if(Dominater > Everule){
+        cout << "Dominater" << "\n";
+    }
+    if(Dominater < Everule){
+        cout << "Everule" << "\n";
+    }
+}
 
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-//     int R1, R2, D1, D2;
-//     cin >> R1 >> R2 >> D1 >> D2;
+    int R1, R2, D1, D2;
+    cin >> R1 >> R2 >> D1 >> D2;
 
-//     solve(R1, R2, D1, D2);
+    solve(R1, R2, D1, D2);
 
-//     return 0;
-// }
+    return 0;
+}
