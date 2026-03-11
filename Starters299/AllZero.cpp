a14a2a8295ad093ed5d78c3fa96182393a7450c9
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-11 21:38:41 +0600

    a simple logic.

---
+++ Starters299/AllZero.cpp
@@ -0,0 +1,32 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve() {
+//     long long A, B, C;
+//     cin >> A >> B >> C;
+
+//     if (C % 3 != 0) {
+//         cout << "No" << "\n";
+//         return;
+//     }
+
+//     if (B == 2 * A + C / 3) {
+//         cout << "Yes" <<"\n";
+//     } else {
+//         cout << "No" << "\n";
+//     }
+// }
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int t;
+//     cin >> t;
+
+//     while (t--) {
+//         solve();
+//     }
+
+//     return 0;
+// }
