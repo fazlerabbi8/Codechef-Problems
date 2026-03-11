6afb5f9fb740b3cb00ff4bf14762371ca68fccb1
Fazle Rabbi <130282376+fazlerabbi8@users.noreply.github.com>
2026-03-11 21:43:23 +0600

    Add files via upload
    simple math logics and standing with some equations

---
+++ Starters299/Vacation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int X, int Y)
+{
+    cout << (X - 1) + Y << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X, Y;
+    cin >> X >> Y;
+
+    solve(X, Y);
+
+    return 0;
+}
