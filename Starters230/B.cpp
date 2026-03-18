304f41df96859e1deaa131af7ce3778e94eadd0d
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-18 22:17:42 +0600

    passed or not.condition is any two sub of greater then 50.

--- Starters230/B.cpp
+++ Starters230/B.cpp
@@ -1,40 +1,40 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// void solve(int x, int y, int z)
-// {
-//     if (x >= 50 && y >= 50)
-//     {
-//         cout << "Yes" << "\n";
-//     }
-//     else if (y >= 50 && z >= 50)
-//     {
-//         cout << "Yes" << "\n";
-//     }
+void solve(int x, int y, int z)
+{
+    if (x >= 50 && y >= 50)
+    {
+        cout << "Yes" << "\n";
+    }
+    else if (y >= 50 && z >= 50)
+    {
+        cout << "Yes" << "\n";
+    }
 
-//     else if (z >= 50 && x >= 50)
-//     {
-//         cout << "Yes" << "\n";
-//     }
-//     else
-//     {
-//         cout << "No" << "\n";
-//     }
-// }
+    else if (z >= 50 && x >= 50)
+    {
+        cout << "Yes" << "\n";
+    }
+    else
+    {
+        cout << "No" << "\n";
+    }
+}
 
-// int main()
-// {
-//     ios::sync_with_stdio(false);
-//     cin.tie(nullptr);
-//     int t;
-//     cin >> t;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
 
-//     while (t--)
-//     {
-//         int x, y, z;
-//         cin >> x >> y >> z;
-//         solve(x, y, z);
-//     }
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        solve(x, y, z);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
