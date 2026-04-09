0df23f7db2c86333e015b48269bc73d77c9c68f6
fazlerabbi13 <mohammad0132794@gmail.com>
2026-04-09 19:46:18 +0600

    minimum cost.

--- Starters233/B.cpp
+++ Starters233/B.cpp
@@ -3,10 +3,30 @@ using namespace std;
 
 void solve()
 {
-    
+    int n, m, a, b, c;
+    cin >> n >> m >> a >> b >> c;
+
+    int ifNisSmall = c * n;
+    int ifMisSmall = c * m;
+    if (n > m)
+    {
+        cout << ifMisSmall + (n - m) * a << "\n";
+    }
+    else
+    {
+        cout << ifNisSmall + (m - n) * b << "\n";
+    }
 }
 
 int main()
 {
-    solve();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
 }
